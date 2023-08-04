05f039939976b63180167904e96a99591e6525f1
Marta Uszakow <hopulinetka@Martas-MacBook-Air.local>
2023-08-04 14:16:05 +0200

    Add game loop

--- Game/TerminalUI.py
+++ Game/TerminalUI.py
@@ -42,9 +42,40 @@ def Displayboard(board: AtomicChessBoard):
     for x in [" ", "A", "B", "C", "D", "E", "F", "G", "H"]:
         print(x, end = " | ")
     print("")
-            
-if __name__ == "__main__":
+    
+def MakeMove(board: AtomicChessBoard):
+    if board.toPlay == 1:
+        print("White to play.")
+    else: print("Black to play.")
+    move = input("Enter your move: ")
+    def translate(s: str):
+        x = 8 - int(s[0])
+        for y, letter in enumerate(["A", "B", "C", "D", "E", "F", "G", "H"]):
+            if s[1].upper() == letter:
+                break
+        return x, y
+    board.SelectSquere(*translate(move[:-2]))
+    m = board.Move(*translate(move[2:]))
+    return m[0], m[1], move.upper()
+        
+def GameLoop():
     atomic = AtomicChessBoard()
     Displayboard(atomic)
+    while True:
+        message = MakeMove(atomic)
+        Displayboard(atomic)
+        print(message[2] + ": " + message[0])
+        if message[1] != None:
+            break
+    if message[1] == 0:
+        print("It is a draw.")
+    elif message[1] == 1:
+        print("White won.")
+    else:
+        print("Black won.")
+            
+            
+if __name__ == "__main__":
+    GameLoop()
 
         
